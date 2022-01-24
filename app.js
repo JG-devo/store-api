f1a9413064df2314d3b5a88d5c4f40a7ce4766f2
Jean Gabriel <67287800+JG-devo@users.noreply.github.com>
2022-01-24 11:25:13 +0000

    Setup basic webserver on app.js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('04 Store API')
+require('dotenv').config();
+// Async errors
+
+const express = require('express');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+// Products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
