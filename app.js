15669dcf3aaed2d74d3ac11a67e80e8a11ca7b41
Jean Gabriel <67287800+JG-devo@users.noreply.github.com>
2022-01-24 12:07:06 +0000

    connect mongo DB and create basic route / controller functions

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const express = require('express');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
@@ -15,6 +17,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
+app.use('/api/v1/products', productsRouter);
+
 // Products route
 
 app.use(notFoundMiddleware);
@@ -25,6 +29,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // connectDB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
